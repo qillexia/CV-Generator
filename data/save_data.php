d04e501516f19197950d9610d16c9d733575eb8f
Haqil Abdillah <72172033+qillexia@users.noreply.github.com>
2025-06-09 22:49:06 +0700

    Add files via upload

---
+++ data/save_data.php
@@ -0,0 +1,22 @@
+<!-- data pribadi -->
+<input type="hidden" name="name" value="<?php echo $_POST['name'] ?? ''; ?>">
+<input type="hidden" name="position" value="<?php echo $_POST['position'] ?? ''; ?>">
+<input type="hidden" name="email" value="<?php echo $_POST['email'] ?? ''; ?>">
+<input type="hidden" name="phone" value="<?php echo $_POST['phone'] ?? ''; ?>">
+<input type="hidden" name="address" value="<?php echo $_POST['address'] ?? ''; ?>">
+<input type="hidden" name="explainSelf" value="<?php echo $_POST['explainSelf'] ?? ''; ?>">
+
+<!-- pendidikan -->
+<input type="hidden" name="title" value="<?php echo $_POST['title'] ?? ''; ?>">
+<input type="hidden" name="institution" value="<?php echo $_POST['institution'] ?? ''; ?>">
+<input type="hidden" name="description" value="<?php echo $_POST['description'] ?? ''; ?>">
+
+<!-- pengalaman -->
+<input type="hidden" name="exPosition" value="<?php echo $_POST['exPosition'] ?? ''; ?>">
+<input type="hidden" name="company" value="<?php echo $_POST['company'] ?? ''; ?>">
+<input type="hidden" name="descCompany" value="<?php echo $_POST['descCompany'] ?? ''; ?>">
+
+<!-- keterampilan -->
+<input type="hidden" name="skill1" value="<?php echo $_POST['skill1'] ?? ''; ?>">
+<input type="hidden" name="skill2" value="<?php echo $_POST['skill2'] ?? ''; ?>">
+<input type="hidden" name="skill3" value="<?php echo $_POST['skill3'] ?? ''; ?>">
